277b33840f33d66ef05d3b9c71809b85686bb1fd
RhexNiebres <rhexniebres@gmail.com>
2025-06-18 19:20:49 +0800

    feat: add mounted state to wait for theme to immediately display buttons with theme

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import data from "../../data/portfolio.json";
 
 const Button = ({ children, type, onClick, classes }) => {
   const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   if (type === "primary") {
     return (
       <button
@@ -13,24 +21,25 @@ const Button = ({ children, type, onClick, classes }) => {
           theme === "dark"
             ? "bg-white text-black border-2 border-slate-100"
             : "bg-black text-white shadow-2xl border-2 border-black"
-        }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
-          data.showCursor && "cursor-none"
-        }  ${classes}`}
+        } transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
+          data.showCursor ? "cursor-none" : ""
+        } ${classes}`}
       >
         {children}
       </button>
     );
   }
+
   return (
     <button
       onClick={onClick}
       type="button"
       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300 ${
         theme === "dark"
-          ? "hover:bg-slate-600 text-white border-2 border-slate-100 "
+          ? "hover:bg-slate-600 text-white border-2 border-slate-100"
           : "hover:bg-slate-100 shadow-2xl border-2 border-black"
-      } hover:scale-105 active:scale-100  tablet:first:ml-0  ${
-        data.showCursor && "cursor-none"
+      } hover:scale-105 active:scale-100 tablet:first:ml-0 ${
+        data.showCursor ? "cursor-none" : ""
       } ${classes} link`}
     >
       {children}
